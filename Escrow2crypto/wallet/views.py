4e4fdfd09f035519a7c8f8a3970b6cc4366fe17c
FawazofLagos <aybest12452gmail.com>
2025-12-30 19:53:34 -0600

    added my api endpoints (SignUP, lOGIN, Wallet balance)

--- Escrow2crypto/wallet/views.py
+++ Escrow2crypto/wallet/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from .models import Wallet
+from .serializers import WalletSerializer
 
 # Create your views here.
+
+class WalletBalanceView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        try:
+            wallet = Wallet.objects.get(user=request.user)
+            serializer = WalletSerializer(wallet)
+            return Response(serializer.data, status=200)
+        except Wallet.DoesNotExist:
+            return Response({'error': 'Wallet not found'}, status=404)
