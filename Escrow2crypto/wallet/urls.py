7d8633099b4d675d19aa1f731b9ca75026be425c
FawazofLagos <aybest12452gmail.com>
2026-01-03 02:17:12 -0600

    Fixed wallets view.py

--- Escrow2crypto/wallet/urls.py
+++ Escrow2crypto/wallet/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import WalletBalanceView
 
 urlpatterns = [
-    path('balance/', WalletBalanceView.as_view(), name='wallet-balance'),
+    path('wallet/balance/', WalletBalanceView.as_view(), name='wallet-balance'),
 ]
