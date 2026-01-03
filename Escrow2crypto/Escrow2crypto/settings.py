50229a1c002f26c100b31e3e3d0bced7514a5583
FawazofLagos <aybest12452gmail.com>
2026-01-02 21:18:20 -0600

    Prepare for deployment

--- Escrow2crypto/Escrow2crypto/settings.py
+++ Escrow2crypto/Escrow2crypto/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(azhmzw64%%+h66+ci@u%dx78ugrv&#u8(o)ql@jex^f(=_j%3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
