ca2bdefdd586ba02dc139c2cbef2ce5eb688621a
FawazofLagos <aybest12452gmail.com>
2026-01-03 11:26:32 -0600

    fixed urls problme

--- Escrow2crypto/users/serializers.py
+++ Escrow2crypto/users/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import User
+from django.contrib.auth.models import User
 
 class SignUpSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
@@ -7,6 +7,9 @@ class SignUpSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['username', 'email', 'password', 'role']
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
 
     def create(self, validated_data):
         user = User.objects.create_user(
