0fd6fc9cb112c6786d7a747de57c6c6040ba63fd
FawazofLagos <aybest12452gmail.com>
2026-01-03 10:54:17 -0600

    updated urls.py in the core project so it recognizes the postman testing

--- Escrow2crypto/Escrow2crypto/urls.py
+++ Escrow2crypto/Escrow2crypto/urls.py
@@ -21,10 +21,10 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('users/', include('users.urls')),
-    path('wallet/', include('wallet.urls')),
-    path('offers/', include('offers.urls')),
-    path('trades/', include('trades.urls')),
+    path('api/', include('users.urls')),
+    path('api/', include('wallet.urls')),
+    path('api/', include('offers.urls')),
+    path('api/', include('trades.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
