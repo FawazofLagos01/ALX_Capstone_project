23a61655e36e63e93a367d28b39e739d70f9797c
FawazofLagos <aybest12452gmail.com>
2026-01-03 20:47:44 -0600

    Fixed wallet so it automatically creates wallet for every new users that signup

--- Escrow2crypto/wallet/models.py
+++ Escrow2crypto/wallet/models.py
@@ -1,10 +1,10 @@
 from django.db import models
-from users.models import User
+from django.conf import settings
 
 # Create your models here.
 
 class Wallet(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     usdt_balance = models.DecimalField(max_digits=12, decimal_places=8, default=0)
 
     def __str__(self):
